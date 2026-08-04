48e832bb55b7fe0a2ad21db727f9df78988ea571
Gabriel Pirovane Miguel <gabriel.balique@gmail.com>
2026-08-04 19:13:12 -0300

    feat: initializing Fastify server with health check

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import Fastify from 'fastify'
+import z, { ZodError } from 'zod'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env/index.js'
+
+export const app = Fastify({ logger: true })
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+})
+
+app.get('/health', async (request, reply) => {
+    return reply.status(200).send({ status: 'ok' })
+})
+
+
+app.setErrorHandler((error, _request, reply) => {
+    if (error instanceof ZodError) {
+        return reply
+            .status(400)
+            .send({ message: z.prettifyError(error).replace('\n ', '') })
+    }
+
+    if (error instanceof SyntaxError) {
+        return reply.status(400).send({
+            message:
+                'O corpo da requisição não está em formato JSON válido. Verifique a estrutura dos dados enviados.',
+        })
+    }
+
+    return reply
+        .status(500)
+        .send({ message: 'Erro interno do servidor!' + error.message })
+})
