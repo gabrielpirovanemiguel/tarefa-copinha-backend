aaff0feb9fb5f90a3dbaea66519588bc92adf13d
Gabriel Pirovane Miguel <gabriel.balique@gmail.com>
2026-08-06 20:23:36 -0300

    feat: add update news route with JWT and admin verification

--- src/http/controllers/admin/news/admin_news_routes.ts
+++ src/http/controllers/admin/news/admin_news_routes.ts
@@ -2,7 +2,9 @@ import type { FastifyInstance } from "fastify";
 import { createNews } from "./create_news_controller.js";
 import { verifyJWT } from "@/http/middlewares/verify-jwt.js";
 import { verifyAdmin } from "@/http/middlewares/verify-admin.js";
+import { updateNews } from "./update_news_controller.js";
 
 export function adminNewsRoutes(app: FastifyInstance) {
     app.post('/', { onRequest: [verifyJWT, verifyAdmin] }, createNews)
+    app.put('/:newsPublicId', { onRequest: [verifyJWT, verifyAdmin] }, updateNews)
 }
